87dcd3d3bd2cf284fd7e201279c395ab92186079
rtompkins95 <rjt8625@rit.edu>
2024-03-11 22:25:55 -0400

    Removing print statement

--- src/main/java/com/pi/Main.java
+++ src/main/java/com/pi/Main.java
@@ -15,8 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(args));
-
         validateArgs(args);
 
         DartBoard dartBoard = buildDartBoard();
